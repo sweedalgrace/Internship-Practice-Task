a16ec644ab31dc3d6ea2a9349e0d1609afbfd478
Sweedal Grace Pinto <sweedalgracepinto@Sweedals-MacBook-Air.local>
2025-07-17 18:49:50 +0530

    Day 1

--- 17.7.25/day1.js
+++ 17.7.25/day1.js
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <script>
 // Global Scope
 var name = "Sweety";
 let age = 22;
@@ -116,7 +107,3 @@ queue.push("One");
 queue.push("Two");
 console.log("Queue shift:", queue.shift());
 
-
-    </script>
-</body>
-</html>
